53d6941e04e66d5a53a2444bfe89a0a274f47c8e
Victor Hagelbäck <victor@hagelback.com>
2018-01-12 07:23:42 +0100

    Refactor Date and Custom indicators to matrix based
    Related Work Items: #6

--- src/indicators/Custom.py
+++ src/indicators/Custom.py
@@ -1,31 +1,57 @@
 import numpy as np
 import pandas as pd
+from typing import Callable, Dict
 
-def miniSharp(data: pd.DataFrame, row: int, period: int):
-    """Calculate MiniSharp for selected period of given ticker data DataFrame 
-    MiniSharp is similar to Williams%R, RSI and Stochastic, but without upper and lower bounds
-    Will use data rows [row:row+period+1]
+from utils.Utils import columnName
+
+def getCustomFeatures() -> Dict[str, Callable[[pd.DataFrame, pd.DataFrame], None]]:
+    """Get custom features
+       Will add miniSharp and pastSharp
+       returns a dictionary of {columnName: lamda (data, feature): feature }
     """
-    _Diff_CpLf = (np.amin(data["Low"][row:row+period+1])-data["Close"][row])/data["Close"][row]
-    _Diff_CpHf = (np.amax(data["High"][row:row+period+1])-data["Close"][row])/data["Close"][row]
-    _CpHf_Less_CpLf = _Diff_CpHf - _Diff_CpLf
-    _ABSofDiff_CpLf = abs(_Diff_CpLf)
-    _ABSofDiff_CpHf = abs(_Diff_CpHf)
-    _KeyValueLong = _ABSofDiff_CpHf/_CpHf_Less_CpLf
-    _KeyValueShort = _ABSofDiff_CpLf/_CpHf_Less_CpLf
-    return _KeyValueLong - _KeyValueShort
 
-def pastSharp(data: pd.DataFrame, row: int, period: int):
+    customFeatures = {}
+    r = range(5, 34+1)
+    # customFeatures.update([columnName("MiniSharp", i), miniSharp(i)] for i in r)  # Forward-looking
+    customFeatures.update([columnName("PastSharp", i), miniSharp(i)] for i in r)
+
+    return customFeatures
+
+def miniSharp(period: int):
+    def miniSharpLambda(data: pd.DataFrame, features: pd.DataFrame):
+        """Calculate --- FORWARD LOOKING --- MiniSharp for selected period of given ticker data DataFrame 
+        MiniSharp is similar to Williams%R, RSI and Stochastic, but without upper and lower bounds
+        Will use data rows [row:row+period+1]
+        """
+        # shifts to make it forward looking
+        _Diff_CpLf = (data["Low"].shift(period+1).rolling(window=period+1).min().shift(-period-1)-data["Close"])/data["Close"]
+        _Diff_CpHf = (data["High"].shift(period+1).rolling(window=period+1).max().shift(-period-1)-data["Close"])/data["Close"]
+        _CpHf_Less_CpLf = _Diff_CpHf - _Diff_CpLf
+        _ABSofDiff_CpLf = _Diff_CpLf.abs()
+        _ABSofDiff_CpHf = _Diff_CpHf.abs()
+        _KeyValueLong = _ABSofDiff_CpHf/_CpHf_Less_CpLf
+        _KeyValueShort = _ABSofDiff_CpLf/_CpHf_Less_CpLf
+        return _KeyValueLong - _KeyValueShort
+    return miniSharpLambda
+
+def pastSharp(period: int):
     """Calculate PastSharp for selected period of given ticker data DataFrame
     PastSharp is similar to Williams%R, RSI and Stochastic, but without upper and lower bounds
     Will use data rows [row-period-1:row+1]
     """
-    _Diff_CpLf = (np.amin(data["Low"][row-period-1:row+1])-data["Close"][row])/data["Close"][row]
-    _Diff_CpHf = (np.amax(data["High"][row-period-1:row+1])-data["Close"][row])/data["Close"][row]
-    _CpHf_Less_CpLf = _Diff_CpHf - _Diff_CpLf
-    _ABSofDiff_CpLf = abs(_Diff_CpLf)
-    _ABSofDiff_CpHf = abs(_Diff_CpHf)
-    _KeyValueLong = _ABSofDiff_CpHf/_CpHf_Less_CpLf
-    _KeyValueShort = _ABSofDiff_CpLf/_CpHf_Less_CpLf
-    return _KeyValueLong - _KeyValueShort
+    def pastSharpLambda(data: pd.DataFrame, features: pd.DataFrame):
+        """Calculate --- FORWARD LOOKING --- MiniSharp for selected period of given ticker data DataFrame 
+        MiniSharp is similar to Williams%R, RSI and Stochastic, but without upper and lower bounds
+        Will use data rows [row:row+period+1]
+        """
+        # shifts to make it forward looking
+        _Diff_CpLf = (data["Low"].rolling(window=period+1).min()-data["Close"])/data["Close"]
+        _Diff_CpHf = (data["High"].rolling(window=period+1).max()-data["Close"])/data["Close"]
+        _CpHf_Less_CpLf = _Diff_CpHf - _Diff_CpLf
+        _ABSofDiff_CpLf = _Diff_CpLf.abs()
+        _ABSofDiff_CpHf = _Diff_CpHf.abs()
+        _KeyValueLong = _ABSofDiff_CpHf/_CpHf_Less_CpLf
+        _KeyValueShort = _ABSofDiff_CpLf/_CpHf_Less_CpLf
+        return _KeyValueLong - _KeyValueShort
+    return pastSharpLambda
 
--- src/indicators/Date.py
+++ src/indicators/Date.py
@@ -1,27 +1,24 @@
 import numpy as np
 import pandas as pd
+from typing import Callable, Dict
 
-def diffCtoH(data: pd.DataFrame, row: int, period: int):
-    """Calculate Difference Close to High[-period] for selected period of given ticker data DataFrame 
-    Will use data rows [row] and [row-period]
+from utils.Utils import columnName
+
+
+def getDateFeatures() -> Dict[str, Callable[[pd.DataFrame, pd.DataFrame], None]]:
+    """Get date-based features
+       returns a dictionary of {columnName: lamda (data, feature): feature }
     """
-    return (data["Close"][row]-data["High"][row-period])/data["High"][row-period]
 
-def diffCtoL(data: pd.DataFrame, row: int, period: int):
-    """Calculate Difference Close to Low[-period] for selected period of given ticker data DataFrame 
-    Will use data rows [row] and [row-period]
-    """
-    return (data["Close"][row]-data["Low"][row-period])/data["Low"][row-period]
-
-def diffCtoO(data: pd.DataFrame, row: int, period: int):
-    """Calculate Difference Close to Open[-period] for selected period of given ticker data DataFrame 
-    Will use data rows [row] and [row-period]
-    """
-    return (data["Close"][row]-data["Open"][row-period])/data["Open"][row-period]
-
-def diffCtoC(data: pd.DataFrame, row: int, period: int):
-    """Calculate Difference Close to Close[-period] for selected period of given ticker data DataFrame 
-    Will use data rows [row] and [row-period]
-    """
-    return (data["Close"][row]-data["Close"][row-period])/data["Close"][row-period]
-
+    return {
+        "Date": lambda data, features: data.index.date,
+        "Date": lambda data, features: data.index.date,
+        "DayOfWeek": lambda data, features: data.index.dayofweek,
+        "DayOfMonth": lambda data, features: data.index.day,
+        "DayOfYear": lambda data, features: data.index.dayofyear,
+        "WeekOfYear": lambda data, features: data.index.weekofyear,
+        "MonthOfYear": lambda data, features: data.index.month,
+        "Time": lambda data, features: data.index.time,
+        "Hour": lambda data, features: data.index.hour,
+        "Minute": lambda data, features: data.index.minute
+    }
